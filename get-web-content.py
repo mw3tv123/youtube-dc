d8307f162812d1c974f2a00e6c692e911d38aaf2
Hung <mw3tv123@gmail.com>
2018-12-26 16:51:49 +0700

    Add search from keyword

--- get-web-content.py
+++ get-web-content.py
@@ -1,20 +1,37 @@
 # Import library
 from __future__ import unicode_literals
 from datetime import datetime
+from bs4 import BeautifulSoup
 import argparse
 import json
-# import requests
 import logging
 import youtube_dl
-
-
-# Variables
-logging.basicConfig(filename='/tmp/getwebcontents_errors.txt', level=logging.DEBUG)
-STORAGE_DIR = '/tmp/my_audio'
-options = {}
+try:
+    # Python 2.x
+    from urllib import quote, request
+except:
+    # Python 3.x
+    from urllib.parse import quote
+    from urllib import request
 
 '''
-=> options <=
+    This Python script enable user to download a YouTube video back to our computer and
+    convert it to any format. This is a tiny project for learning new thing from Python.
+
+                    |-----------------------------|
+                    | Module used in this project |
+                    |-----------------------------|
+
+        - datetime          --> For get system time.
+        - bs4/BeautifulSoup --> For working with html contents.
+        - argparse          --> For enable parse arguments through CLI.
+        - json              --> For working with json data.
+        - logging           --> For save error information into a file for later analyse.
+        - youtube-dl        --> For working with video clip in YouTube.
+        - urllib            --> For working with search url.
+'''
+'''
+    => options <=
     YouTube-dl options when download audio file. It contains the configuration of how
     YouTube-dl handle data, file location, log handler,... You may want to take a look
     at Youtube-dl options pages for more information about how to configuration it.
@@ -22,6 +39,10 @@ options = {}
     # More options can be found in:
         https://github.com/rg3/youtube-dl/blob/3e4cedf9e8cd3157df2457df7274d0c842421945/youtube_dl/YoutubeDL.py#L137-L312
 '''
+# Variables
+logging.basicConfig(filename='./error_logs.txt', level=logging.DEBUG)
+_DEFAULT_STORAGE_DIR = './my_audio/%(title)s.%(ext)s'
+options = {}
 
 
 class LogHandler(object):
@@ -69,32 +90,16 @@ def parse_destination_url():
                         )
     parser.add_argument('-f', '--file',
                         type=str,
-                        default=STORAGE_DIR,
+                        default=_DEFAULT_STORAGE_DIR,
                         nargs='?',
                         help='Path to store audio file'
                         )
+    parser.add_argument('-k', '--keyword',
+                        type=str,
+                        nargs='?',
+                        help='Special a keyword to search in YouTube and return list of result'
+                        )
     return parser.parse_args()
-
-
-# def create_connection(url):
-#     return requests.get(url)
-
-
-# def parse_html_content(response):
-#     return response.text
-
-
-# def store_download_file(data, file=''):
-#     try:
-#         f = open(file, 'w')
-#         if type(data) == dict:
-#             json.dumps(data, file)
-#         else:
-#             f.write(data)
-#         f.close()
-#         logging.info('{0} - Store data completed!'.format(datetime.now()))
-#     except Exception as e:
-#         logging.error('{0} - Encountered error: {1}'.format(datetime.now(), e))
 
 
 def load_config(**kwargs):
@@ -106,7 +111,6 @@ def load_config(**kwargs):
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'outtmpl': '/tmp/my_audio/%(title)s.%(ext)s',
         'restrictfilenames': True,
         'logger': LogHandler(),
         'progress_hooks': [progress_handler],
@@ -130,6 +134,16 @@ def load_config(**kwargs):
                 logging.error('{0} - Encountered error: {1}'.format(datetime.now(), e))
 
 
+def search_by_keywords(keyword=''):
+    query = quote(keyword)
+    search_url = "https://www.youtube.com/results?search_query=" + query
+    response = request.urlopen(search_url)
+    html = response.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    for video in soup.find_all(attrs={'class': 'yt-uix-tile-link'}):
+        print('https://www.youtube.com' + video['href'])
+
+
 def download_video(url):
     print('\n\n')
     with youtube_dl.YoutubeDL(options) as ydl:
@@ -140,24 +154,38 @@ def download_video(url):
                 ydl.download([element])
 
 
-def main():
-    # print(parse_html_content(create_connection('https://www.googleapis.com/youtube/v3/search')))
-    args = parse_destination_url()
-    path = ''
-    if args.config:
-        path = args.config
-    load_config(path=path)
-    # if options is None:
-    #     logging.error('{0} - Encountered error: Option file not existed!'.format(datetime.now()))
-    #     return 0
-
-    if args.url is not None:
-        download_video(args.url)
+def user_interface():
+    print('{:^50}'.format('Easy YouTube Downloader'))
+    print('What do you want?')
+    print('{0:^20}{1:^30}'.format('(1) Search from keyword', '(2) Download from link'))
+    user_choose = int(input())
+    if user_choose == 1:
+        keyword = input('>>> Enter the keyword: ')
+        print('{:^40}'.format('==> RESULT <=='))
+        search_by_keywords(keyword)
     else:
-        print('{0:^50}'.format('Easy YouTube Downloader'))
-        url = input(">>> Enter the URL of the video: ")
+        url = input('>>> Enter the URL of the video: ')
         download_video(url)
 
+
+def main():
+    args = parse_destination_url()
+
+    path = ''
+    audio_folder = ''
+    if args.config:
+        path = args.config
+    if args.file:
+        audio_folder = args.file
+
+    load_config(path=path, file=audio_folder)
+
+    if args.keyword:
+        search_by_keywords(args.keyword)
+    elif args.url is not None:
+        download_video(args.url)
+    else:
+        user_interface()
     print('\b\r\r[DONE]')
 
 
